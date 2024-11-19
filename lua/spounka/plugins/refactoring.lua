334cad091f7bbce6ca759f517c8e47f5bff442bb
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-11-19 17:45:14 +0100

    fixed refactoring plugin and config

--- lua/spounka/plugins/refactoring.lua
+++ lua/spounka/plugins/refactoring.lua
@@ -3,12 +3,12 @@ local defines = require("spounka.defines")
 return {
   "ThePrimeagen/refactoring.nvim",
   lazy = true,
-  ft = defines.REFACTOR_FILES_TYPES,
+  ft = defines.REFACTOR_LAZY_FILES_TYPES,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
   config = function()
-    require("refactoring").setup()
+    require("refactoring").setup({})
   end,
 }
