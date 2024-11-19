b9ae85200450cfac521bf4220bd9b13b8c925ab2
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-11-19 08:36:39 +0100

    added fugitive push shortcut

---
+++ lua/spounka/keyboard/fugitive.lua
@@ -0,0 +1,1 @@
+vim.keymap.set({ "n", "x" }, "<leader>gp", "<cmd>:G push<CR>")
--- lua/spounka/keyboard/init.lua
+++ lua/spounka/keyboard/init.lua
@@ -1,4 +1,5 @@
 require("spounka.keyboard.buffers")
 require("spounka.keyboard.debug")
 require("spounka.keyboard.lsp")
+require("spounka.keyboard.fugitive")
 return {}
