b9a250bab404f96ce2a867c7aa17e945191c3ae5
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-12-07 18:52:11 +0100

    added markdown to conform linting

--- lua/spounka/plugins/conform.lua
+++ lua/spounka/plugins/conform.lua
@@ -23,6 +23,7 @@ return {
       -- Conform can also run multiple formatters sequentially
       python = { "isort", "black" },
       go = {},
+      markdown = { "markdownlint" },
       --
       -- You can use "stop_after_first" to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
