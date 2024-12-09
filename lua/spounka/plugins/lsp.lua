9069e7a65f0830236ca59ac1cbe1f6e448a3393d
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-12-09 08:56:18 +0100

    lsp inlay hints binding update

--- lua/spounka/plugins/lsp.lua
+++ lua/spounka/plugins/lsp.lua
@@ -110,9 +110,9 @@ return {
           --
           -- This may be unwanted, since they displace some of your code
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map("<leader>th", function()
+            map("<leader>lth", function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-            end, "[T]oggle Inlay [H]ints")
+            end, "[L]sp [T]oggle Inlay [H]ints")
           end
         end,
       })
