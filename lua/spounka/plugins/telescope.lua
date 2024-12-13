d4758559bbe6f78451b731b9aa507c4b3606d072
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-12-13 12:19:59 +0100

    native fzf searching in telescope

--- lua/spounka/plugins/telescope.lua
+++ lua/spounka/plugins/telescope.lua
@@ -59,6 +59,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
         },
       },
       extensions = {
+        fzf = {},
         ["ui-select"] = {
           require("telescope.themes").get_dropdown(),
         },
