2e0e0044aa759c8febb6f92385446e69677e87aa
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-12-13 13:10:29 +0100

    added clangd switch header source keymap

--- lua/spounka/keyboard/lsp.lua
+++ lua/spounka/keyboard/lsp.lua
@@ -5,3 +5,7 @@ end, { silent = true, desc = "[P]roject [L]sp" })
 vim.keymap.set({ "n", "x" }, "<Leader>ps", function()
   vim.cmd("LspStop")
 end, { silent = true, desc = "[P]roject [S]top LSP" })
+
+vim.keymap.set({ "n", "x", "v" }, "<M-o>", function()
+  vim.cmd("ClangdSwitchSourceHeader")
+end, { silent = false, desc = "Switch Header Source Files" })
