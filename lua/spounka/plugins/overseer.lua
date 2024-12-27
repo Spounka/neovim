0bbe99a5e5c0f5d8ae2f781d3eb67690c5a2249e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-12-27 10:47:46 +0100

    chore: lazy load overseer plugin

--- lua/spounka/plugins/overseer.lua
+++ lua/spounka/plugins/overseer.lua
@@ -1,5 +1,8 @@
+local defines = require("spounka.defines")
 return {
   "stevearc/overseer.nvim",
+  ft = defines.TREE_SITTER_ENABLED_LANGUAGES,
+  lazy = true,
   opts = {},
   config = function()
     local overseer = require("overseer")
