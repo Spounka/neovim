b621417fb82f6d6227345f0a7e5efe408bd590d9
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2025-03-28 14:13:32 +0100

    chore: disable codeium ai assistant

--- lua/spounka/plugins/codeium.lua
+++ lua/spounka/plugins/codeium.lua
@@ -2,6 +2,7 @@ local defines = require("spounka.defines")
 return {
   "Exafunction/codeium.nvim",
   lazy = true,
+  cond = false,
   ft = defines.REFACTOR_LAZY_FILES_TYPES,
   dependencies = {
     "nvim-lua/plenary.nvim",
