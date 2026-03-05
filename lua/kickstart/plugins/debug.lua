337bb5393d9425c10105d7ae2974d779e2b60d7e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2026-03-05 10:07:00 +0100

    debug: remove unneeded debug plugins

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -206,12 +206,9 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        "delve",
         "typescript",
         "javascript",
         "codelldb",
-        "go",
-        unpack(defines.DEBUG_LAZY_FILE_TYPES),
       },
     })
 
